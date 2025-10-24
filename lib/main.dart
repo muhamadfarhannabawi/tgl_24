7ad00234809c20bb9855416d22a18bcb8407fa26
muhamadfarhannabawi <1123150170@global.ac.id>
2025-10-24 10:37:57 +0700

    update code file

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
+//Todo
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(Myapp());
+  runApp(MyApp());
 }
 
-class Myapp extends StatelessWidget {
-  const Myapp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Latihan container",
+      title: 'Latihan Container',
       debugShowCheckedModeBanner: false,
       home: Halaman(),
     );
@@ -24,21 +26,43 @@ class Halaman extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.amberAccent,
-        title: Text("Ini halaman depan"),
-
+        backgroundColor: Colors.deepPurple,
+        surfaceTintColor: Colors.blue,
+        title: const Text('Halaman Depan'),
+        shadowColor: Colors.black,
         actions: [
-          Icon(Icons.person),
-          SizedBox(width: 10),
-          Icon(Icons.account_balance),
-          SizedBox(width: 5),
+          IconButton(
+            onPressed: () {
+              // Aksi ketika ikon pencarian ditekan
+              print('Pencarian ditekan');
+            },
+            icon: const Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {
+              // Aksi ketika ikon lebih banyak ditekan
+              print('Lebih banyak ditekan');
+            },
+            icon: const Icon(Icons.more_vert),
+          ),
         ],
       ),
       body: Container(
-        width: 100,
-        height: double.infinity,
-        color: Colors.blue,
-        alignment: Alignment.topCenter,
+        // color: Colors.lightBlueAccent,
+        alignment: Alignment.center,
+        // width: 200,
+        // height: 200,
+        // child: Row(children: [Text("text")]),
+        margin: EdgeInsets.lerp(EdgeInsets.all(20), EdgeInsets.all(40), 0.5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.lightBlueAccent,
+          gradient: LinearGradient(
+            colors: [Colors.deepPurple, Colors.black],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          ),
+        ),
       ),
     );
   }
